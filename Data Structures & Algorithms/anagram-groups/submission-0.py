c6db1900728a313760e761f1f26aa9e02b879c1b
Pyae Sone <110600456+PyaesoneP@users.noreply.github.com>
2026-06-29 17:47:07 +0800

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        seen = {}
+        for st in strs:
+            sorted_st = "".join(sorted(st))
+            if sorted_st in seen:
+                seen[sorted_st].append(st)
+            else:
+                seen.setdefault(sorted_st, []).append(st)
+
+        return list(seen.values())
